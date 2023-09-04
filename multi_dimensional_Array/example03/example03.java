decd267f14687019b5c128674fbb083debabf140
theNum1Developer <thenum1developer@gmail.com>
2023-09-04 13:27:15 +0900

    feat: add multi_dimensional_Array.java

---
+++ multi_dimensional_Array/example03/example03.java
@@ -0,0 +1,16 @@
+public class example03 {
+	public static void main(String[] args) {
+		int[][] arr = {
+			{10 , 20},
+			{10, 20, 30, 40},
+			{10}
+		};
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
